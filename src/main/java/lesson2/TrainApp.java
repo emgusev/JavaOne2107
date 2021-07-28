39e150422f1c544a3a53b47d0700dbfbc029fa05
Evgeniy Gusev <emgusev@yandex-team.ru>
2021-07-28 22:10:40 +0300

    lesson 3

--- src/main/java/lesson2/TrainApp.java
+++ src/main/java/lesson2/TrainApp.java
@@ -4,7 +4,7 @@ public class TrainApp {
     public static void main(String[] args) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if ((i+j) % 2 ==1 ) {
+                if ((i + j) % 2 == 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("0 ");
@@ -12,5 +12,22 @@ public class TrainApp {
             }
             System.out.println();
         }
+    }
+
+    public static boolean isSummOfIntsInRage(int a, int b, int leftRange, int rightRange) {
+        return true;
+    }
+
+
+    // Не надо так делять. Позязя
+    public static boolean isTrueOrFalse(int a, int b, int c, int d) {
+        return false;
+    }
+
+    public static boolean isPositive(int n) {
+        if (n >= 0) {
+            return true;
+        }
+        return false;
     }
 }
